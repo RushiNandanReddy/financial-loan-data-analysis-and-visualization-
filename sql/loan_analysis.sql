73826599c9de26a1371e38514b481d9c9f797ffd
Rushi Nandan Reddy <rushinandanreddy123@gmail.com>
2026-06-03 12:52:14 +0530

    Add files via upload

---
+++ sql/loan_analysis.sql
@@ -0,0 +1,105 @@
+SELECT * FROM bank_loan_data
+
+#LOAN STATUS#
+SELECT
+        loan_status,
+        COUNT(id) AS LoanCount,
+        SUM(total_payment) AS Total_Amount_Received,
+        SUM(loan_amount) AS Total_Funded_Amount,
+        AVG(int_rate * 100) AS Interest_Rate,
+        AVG(dti * 100) AS DTI
+    FROM
+        bank_loan_data
+    GROUP BY
+        loan_status
+
+#MONTH LOAN STATUS#
+SELECT
+        loan_status,
+        SUM(total_payment) AS M_Total_Amount_Received,
+        SUM(loan_amount) AS M_Total_Funded_Amount
+    FROM
+        bank_loan_data
+    WHERE MONTH(issue_date) = 12
+    GROUP BY
+        loan_status
+
+B.#BANK LOAN REPORT OVERVIEW#
+SELECT 
+	MONTH(issue_date) AS Month_Munber, 
+	DATENAME(MONTH, issue_date) AS Month_name, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY MONTH(issue_date), DATENAME(MONTH, issue_date)
+ORDER BY MONTH(issue_date)
+
+#STATE#
+SELECT 
+	address_state AS State, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY address_state
+ORDER BY address_state
+
+#TERM#
+SELECT 
+	term AS TERM, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY term
+ORDER BY term
+
+#EMPLOYEE LENGTH#
+SELECT 
+	emp_length AS EMPLOYEE_LENGTH, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY emp_length
+ORDER BY emp_length
+
+#PURPOSE#
+SELECT 
+	purpose AS PURPOSE, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY purpose
+ORDER BY purpose
+
+#HOME OWNERSHIP#
+SELECT 
+	home_ownership AS Home_Ownership, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY home_ownership
+ORDER BY home_ownership
+
+#PURPOSE FOR GRADE#
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
